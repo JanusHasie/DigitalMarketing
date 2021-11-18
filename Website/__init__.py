1c3c13e44bbffaa6af0679923140d4798b4ba6d9
JanusHasie <janushaasbroek@gmail.com>
2021-11-18 12:32:54 +0200

    add jinja

--- Website/__init__.py
+++ Website/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app() :
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '1234'
+    app.config['SECRET_KEY'] = '1234' 
 
     from .views import views
     from .auth import auth
