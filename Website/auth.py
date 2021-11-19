3c384d590f13951dc00b2482f46e9c7766a4339c
JanusHasie <janushaasbroek@gmail.com>
2021-11-19 13:32:39 +0200

    update

--- Website/auth.py
+++ Website/auth.py
@@ -1,16 +1,32 @@
 #Authorisation, security
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login() :
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout() :
     return render_template("login.html")
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods = ['GET', 'POST'])
 def sign_up() :
+    if request.method == 'POST' :
+        email = request.form.get('email')
+        firstname = request.form.get('firstName')
+        password1 = request.form.get('password')
+        password2 = request.form.get('cpassword')
+
+        if len(email) < 4:
+            flash('Email longer', category='issue')
+        elif len(firstname) < 2:
+            flash('Email much longer', category='issue')
+        else :
+            flash('This is okay', category='good')
+
     return render_template("sign_up.html")
