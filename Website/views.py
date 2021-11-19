f255d7d83f1266576f2001e7f4bde6213212befd
JanusHasie <janushaasbroek@gmail.com>
2021-11-19 15:22:36 +0200

    update

--- Website/views.py
+++ Website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 
 views = Blueprint('views', __name__)
 
@@ -6,7 +6,7 @@ views = Blueprint('views', __name__)
 def home() :
     return render_template('home.html')
 
-# , request, flash, jsonify
+# 
 # from flask_login import login_required, current_user
 # from .models import Note
 # from . import db
